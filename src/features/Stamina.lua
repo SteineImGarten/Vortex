47809ae59ea2574ff3bc4bc21dd26aee8cd4a6d6
SteineImGarten <hausslermichael414@gmail.com>
2026-06-10 10:31:27 +0200

    Refactor Stamina.Init to use Vortex framework
    Updated stamina initialization to use Vortex framework and modified stamina parameters.

--- src/features/Stamina.lua
+++ src/features/Stamina.lua
@@ -5,23 +5,23 @@
 
 local Stamina = {}
 
-function Stamina.Init(FrameWork)
+function Stamina.Init(Vortex)
     -- Retrieve default stamina handler using simplified framework call method
-    local DefaultStamina = FrameWork.Call("@DefaultStaminaHandlerClient", "getDefaultStamina")
+    local DefaultStamina = Vortex.Call("@DefaultStaminaHandlerClient", "getDefaultStamina")
     if not DefaultStamina then
         warn("[Stamina] DefaultStamina handler instance not found!")
         return
     end
 
     -- Boost Base Max Stamina & set current stamina level
-    FrameWork.Call("@Stamina", "setBaseMaxStamina", DefaultStamina, 150)
-    FrameWork.Call("@Stamina", "setStamina", DefaultStamina, 1)
+    Vortex.Call("@Stamina", "setBaseMaxStamina", DefaultStamina, 350)
+    Vortex.Call("@Stamina", "setStamina", DefaultStamina, 200)
 
     -- Configure gain delay and rates
     print(("[Stamina] Initial Gain Delay: %s | Gain Rate: %s"):format(tostring(DefaultStamina.gainDelay), tostring(DefaultStamina.gainPerSecond)))
     
-    DefaultStamina.gainDelay = 0.25
-    DefaultStamina.gainPerSecond = 35
+    DefaultStamina.gainDelay = 0.05
+    DefaultStamina.gainPerSecond = 75
     
     print(("[Stamina] Modified Gain Delay: %s | Gain Rate: %s"):format(tostring(DefaultStamina.gainDelay), tostring(DefaultStamina.gainPerSecond)))
 end
